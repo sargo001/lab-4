affe737c4059a005bbf3b5cfb62ddfe9c276abb9
sargo001 <61858818+sargo001@users.noreply.github.com>
2021-06-26 18:49:18 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace laba_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа 4.");
+            int v = 32;
+            Console.WriteLine(+((v % 9) + 1) + " Вариант");
+            Console.Write("РАЗМЕР МАССИВА= ");
+            var len = int.Parse(Console.ReadLine());
+            var array = new int[len];
+            Console.Write("N= ");
+            var n = int.Parse(Console.ReadLine());
+            Console.Write("K= ");
+            var k = int.Parse(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+
+            {
+                Console.Write("Элемент (" + i + ") = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Заменить что кратно N на K");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % n == 0)
+                {
+                    array[i] = k;
+
+                }
+                Console.WriteLine(array[i]);
+            }
+
+           
+            
+
+
+
+        }
+    }
+}
